041d79aba356efd4f989ff6c9491195f929707e4
karam-380dev <160220362+karam-baii@users.noreply.github.com>
2024-08-05 03:29:52 +0100

    Add files via upload

---
+++ log.js
@@ -0,0 +1,8 @@
+let RequestCode = document.getElementById("Request");
+let PasswordText = document.getElementById("PasswordText");
+let MyInput = document.getElementById("Password");
+
+function change () {
+    PasswordText.innerText = "Code";
+    MyInput.placeholder= "Enter your code";
+}
