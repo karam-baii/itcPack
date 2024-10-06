1adb50edb8fec34435fda4be1a06709dc45033be
karam-380dev <160220362+karam-baii@users.noreply.github.com>
2024-10-06 18:22:57 +0100

    Update log.js

--- log.js
+++ log.js
@@ -1,8 +1,18 @@
-let RequestCode = document.getElementById("Request");
+let RequestCodeButton = document.getElementById("Request");
 let PasswordText = document.getElementById("PasswordText");
 let MyInput = document.getElementById("Password");
+let textAdmin = document.getElementById("textAdmin");
 
-function change () {
-    PasswordText.innerText = "Code";
-    MyInput.placeholder= "Enter your code";
-}
+RequestCodeButton.addEventListener("click" , () => {
+    if (PasswordText.innerText === "Code") {
+        PasswordText.innerText = "Password";
+        MyInput.placeholder = "Enter your password";
+        RequestCodeButton.innerText = "Request access";
+        textAdmin.style.opacity = "1";
+    } else {
+        PasswordText.innerText = "Code";
+        MyInput.placeholder = "Enter your code";
+        RequestCodeButton.innerText = "i Have an account";
+        textAdmin.style.opacity = "0.1";
+        } 
+})
